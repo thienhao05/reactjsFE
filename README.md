28ade81fe05c38c9685a0eac3935b0f9d593d299
phao9350@gmail.com <phao9350@gmail.com>
2025-12-20 21:05:25 +0700

    update buoi 7

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+# NOTE
+
+Mình dùng hook thì nó sẽ tiện lợi hơn, nhưng hook chỉ dùng trong functional nha
--- src/App.js
+++ src/App.js
@@ -1,63 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function App(props) {
-  console.log("component re-render");
+  const money = useSelector((state) => state.money);
+  //console.log("changed"); //khi nào mà state của mình change thì cái này sẽ hiện lên
+  const dispatch = useDispatch(); //đẩy action lên trên store
+  function dispatchDeposit(number) {
+    dispatch({ type: "deposit", payload: number });
+  }
 
+  function dispatchWithdraw(number) {
+    dispatch({ type: "withdraw", payload: number });
+  }
   return (
     <div>
-      <button onClick={() => props.dispatchDeposit(20)}>deposit $20</button>
-      <button onClick={() => props.dispatchWithdraw(10)}>withdraw $10</button>
-      <div id="result">{props.money}</div>
+      <button onClick={() => dispatchDeposit(20)}>deposit $20</button>
+      <button onClick={() => dispatchWithdraw(10)}>withdraw $10</button>
+      <div>{money}</div>
       {/* <div>{props.increment}</div> */}
     </div>
   );
 }
 
-//gõ map
-
-//ownProps là {increment: 5}
-
-const mapStateToProps = (state, ownProps) => {
-  //money của state trên store mà change thì component sẽ được re-render
-  //nghĩa là trong giao diện của mình có nhiều thuộc tính, khi mà có 1
-  //cái nào đó bị thay đổi thì nó sẽ không render lại tất cả mà chỉ render
-  //cái component nào thay đổi thôi
-  //ownProps
-  //hàm mapStateToProps luôn luôn chạy để nó mới kiểm tra đc
-  //cái state nó bị change và khi mà cái state nó bị change thì lúc đó nó
-  //sẽ re-render lại cái này => nó luôn chạy nếu có state bất kì thay đổi
-  //có nghĩa là nó luôn luôn chạy, có chạy hay không là phụ thuộc vào, cái giá trị của cái
-  //thuộc tính này có tạo hay không
-  //có nghĩa là cái hàm này luôn luôn lắng nghe cái stateChange đó
-  //nó phải luôn luôn chạy thì nó mới biết trong đây có change hay không
-  //và khi mà cái này có change thì nó mới đc re-render lại
-  // if(ownProps.increment == 5){
-  //   return {teoem: state.teoem}
-  // }
-  console.log("chạy hàm mapStateToProps");
-  return {
-    money: state.money,
-    // money: 1, //test thử số 1 thì web của mình mặc định là 1 luôn
-    //nên nó sẽ không có re-render lại nha
-    // teoem: 222
-  };
-};
-
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    dispatchDeposit: (number) => {
-      dispatch({ type: "deposit", payload: number });
-    },
-    dispatchWithdraw: (number) => {
-      // dispatch({ type: "withdraw1", payload: number });
-      dispatch({ type: "withdraw", payload: number });
-    },
-  };
-};
-
-export default connect(
-  mapStateToProps, //
-  mapDispatchToProps
-)(App);
+//mình mà dùng hook thì nó sẽ tiện lợi hơn
+//cách này thì mình sẽ không sài connect
+export default App;
